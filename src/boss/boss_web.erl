21c85187cd638e20020b82ed3a1e89c138aadcf5
fylfot <mr.fylfot@gmail.com>
2018-07-02 04:07:58 +0300

    Make timeout for chicagoboss 5 -> 25s

--- src/boss/boss_web.erl
+++ src/boss/boss_web.erl
@@ -12,6 +12,8 @@
 
 -module(boss_web).
 
+-define(TIMEOUT, 25000).
+
 -export([reload_routes/0,
         reload_translation/1,
         reload_all_translations/0,
@@ -27,40 +29,40 @@
         application_info/1]).
 
 reload_routes() ->
-    gen_server:call(boss_web, reload_routes).
+    gen_server:call(boss_web, reload_routes, ?TIMEOUT).
 
 reload_translation(Locale) ->
-    gen_server:call(boss_web, {reload_translation, Locale}).
+    gen_server:call(boss_web, {reload_translation, Locale}, ?TIMEOUT).
 
 reload_all_translations() ->
-    gen_server:call(boss_web, reload_all_translations).
+    gen_server:call(boss_web, reload_all_translations, ?TIMEOUT).
 
 reload_init_scripts() ->
-    gen_server:call(boss_web, reload_init_scripts).
+    gen_server:call(boss_web, reload_init_scripts, ?TIMEOUT).
 
 get_all_routes() ->
-    gen_server:call(boss_web, get_all_routes).
+    gen_server:call(boss_web, get_all_routes, ?TIMEOUT).
 
 get_all_models() ->
-    gen_server:call(boss_web, get_all_models).
+    gen_server:call(boss_web, get_all_models, ?TIMEOUT).
 
 get_all_applications() ->
-    gen_server:call(boss_web, get_all_applications).
+    gen_server:call(boss_web, get_all_applications, ?TIMEOUT).
 
 base_url(App) ->
-    gen_server:call(boss_web, {base_url, App}).
+    gen_server:call(boss_web, {base_url, App}, ?TIMEOUT).
 
 domains(App) ->
-    gen_server:call(boss_web, {domains, App}).
+    gen_server:call(boss_web, {domains, App}, ?TIMEOUT).
 
 static_prefix(App) ->
-    gen_server:call(boss_web, {static_prefix, App}).
+    gen_server:call(boss_web, {static_prefix, App}, ?TIMEOUT).
 
 translator_pid(AppName) ->
-    gen_server:call(boss_web, {translator_pid, AppName}).
+    gen_server:call(boss_web, {translator_pid, AppName}, ?TIMEOUT).
 
 router_pid(AppName) ->
-    gen_server:call(boss_web, {router_pid, AppName}).
+    gen_server:call(boss_web, {router_pid, AppName}, ?TIMEOUT).
 
 application_info(App) ->
-    gen_server:call(boss_web, {application_info, App}).
+    gen_server:call(boss_web, {application_info, App}, ?TIMEOUT).
